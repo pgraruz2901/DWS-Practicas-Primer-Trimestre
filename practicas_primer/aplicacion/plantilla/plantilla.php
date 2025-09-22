63ba25947ccaad79474ee992ede1d171f37563f3
pgraruz2901 <pgraruz2901@g.educaand.es>
2025-09-22 14:22:59 +0200

    cambios

---
+++ practicas_primer/aplicacion/plantilla/plantilla.php
@@ -0,0 +1,91 @@
+<?php
+function paginaError(string $mensaje)
+{
+    header("HTTP/1.0 404 $mensaje");
+    inicioCabecera("PRACTICA");
+    finCabecera();
+    inicioCuerpo("ERROR");
+    echo "<br />\n";
+    echo $mensaje;
+    echo "<br />\n";
+    echo "<br />\n";
+    echo "<br />\n";
+    echo "<a href='/index.php'>Ir a la pagina principal</a>\n";
+
+    finCuerpo();
+}
+function inicioCabecera($titulo)
+{
+?>
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <meta charset="utf-8">
+        <!-- Always force latest IE rendering engine (even in
+intranet) & Chrome Frame
+ Remove this if you use the .htaccess -->
+        <meta http-equiv="X-UA-Compatible"
+            content="IE=edge,chrome=1">
+        <title><?php echo $titulo ?></title>
+        <meta name="description" content="">
+        <meta name="author" content="Administrador">
+        <meta name="viewport" content="width=device-width; initialscale=1.0">
+        <!-- Replace favicon.ico & apple-touch-icon.png in the root
+of your domain and delete these references -->
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+        <link rel="stylesheet" type="text/css"
+            href="/estilos/base.css">
+    <?php
+}
+function finCabecera()
+{
+    ?>
+    </head>
+<?php
+}
+function inicioCuerpo(string $cabecera)
+{
+    global $acceso;
+?>
+
+    <body>
+        <div id="documento">
+
+            <header>
+                <h1 id="titulo"><?php echo $cabecera; ?></h1>
+            </header>
+
+            <div id="barraLogin">
+
+            </div>
+            <div id="barraMenu">
+                <ul>
+                    <li><a href="/index.php">Inicio</a></li>
+                    <li><a href="/aplicacion/pruebas">pruebas</a></li>
+                </ul>
+            </div>
+
+            <div>
+            <?php
+        }
+        function finCuerpo()
+        {
+            ?>
+                <br />
+                <br />
+            </div>
+            <footer>
+                <hr width="90%" />
+                <div>
+                    &copy; Copyright by Profesor
+                </div>
+            </footer>
+        </div>
+    </body>
+
+    </html>
+<?php
+        }
---
+++ practicas_primer/aplicacion/pruebas/index.php
@@ -0,0 +1,31 @@
+<?php
+include_once(dirname(__FILE__) . "/../../cabecera.php");
+//controlador
+
+
+
+//dibuja la plantilla de la vista
+inicioCabecera("APLICACION PRUEBA");
+cabecera();
+finCabecera();
+
+inicioCuerpo("APLICACION PRUEBA");
+cuerpo(); //llamo a la vista
+finCuerpo();
+
+
+
+// **********************************************************
+
+//vista
+function cabecera() {}
+
+//vista
+function cuerpo()
+{
+?>
+
+    <a href="/aplicacion/pruebas/sintaxisBasica.php">Sintaxis Basica</a>
+<?php
+
+}
